ea05e208186fc1044e06515fdc4a42462a3b5f75
javierezequielfarias <javierezequielfarias@gmail.com>
2023-12-05 23:13:10 -0300

    Primer preentrega js

---
+++ js/preentrega_uno.js
@@ -0,0 +1,116 @@
+//calculador de promedios alumnos 
+
+let nota1=0
+let nota2=0
+let nota3=0
+let suma=0
+let prom=0
+
+console.log ('holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+
+nota1 = prompt ('ingrese nota1')
+nota2 = prompt ('ingrese nota1')
+nota3 = prompt ('ingrese nota1')
+
+suma = nota1 + nota2 + nota3
+prom = suma/3
+
+console.log ('el promedio es: ' + prom)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
