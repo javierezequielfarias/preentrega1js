32f416020ca0d64ff8dab4b278324de60ac65a94
javierezequielfarias <javierezequielfarias@gmail.com>
2023-12-09 12:23:44 -0300

    programa con funcion

--- js/preentrega_uno.js
+++ js/preentrega_uno.js
@@ -1,24 +1,35 @@
-//calculador de promedios alumnos 
-
-let nota1=0
-let nota2=0
-let nota3=0
-let suma=0
-let prom=0
-
-console.log ('holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-
-nota1 = prompt ('ingrese nota1')
-nota2 = prompt ('ingrese nota1')
-nota3 = prompt ('ingrese nota1')
-
-suma = nota1 + nota2 + nota3
-prom = suma/3
-
-alert ('el promedio es: ' + prom)
+let alumno
+let continuar = 's'
+let nota
+let sumatoria = 0
+let promedio = 0
+let mejorAlumno
+let mejorPromedio = 0
 
 
+while (continuar == 's') {
+    alumno = prompt('ingrese nombre')
+    for (let i = 0; i < 3; i++) {
+        nota = Number(prompt('ingrese nota'))
+        sumatoria += nota
+    }
+    //promedio = sumatoria / 3
+    promedio = calcular (sumatoria)
+    if (promedio > mejorPromedio) {
+        mejorPromedio = promedio
+        mejorAlumno = alumno
+    }
+    sumatoria = 0
+    console.log(promedio)
+    continuar = prompt('desea continuar s/n')
+}
+console.log(mejorPromedio)
+alert('el alumno ' + mejorAlumno + ' tiene el mejor promedio , el cual es de: ' + mejorPromedio)
 
+function calcular (suma){
+    let resultado = suma/3
+    return resultado
+}
 
 
 
